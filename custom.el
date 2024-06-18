ae394f401f28fbfde0eb21cb80eb6077f3738734
liangl <liispon@gmail.com>
2024-06-18 22:09:12 +0800

    line number

--- custom.el
+++ custom.el
@@ -12,7 +12,7 @@
 (setq centaur-server t)                      ; Enable `server-mode' or not: t or nil
 (setq centaur-icon nil)                        ; Display icons or not: t or nil
 (setq centaur-package-archives 'sjtu)         ; Package repo: melpa, emacs-cn, bfsu, netease, sjtu, tencent, tuna or ustc
-(setq centaur-theme 'sanityinc-tomorrow-night)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
+;; (setq centaur-theme 'sanityinc-tomorrow-night)                     ; Color theme: auto, random, system, default, pro, dark, light, warm, cold, day or night
 ;; (setq centaur-completion-style 'minibuffer)    ; Completion display style: minibuffer or childframe
 ;; (setq centaur-dashboard nil)                   ; Display dashboard at startup or not: t or nil
 (setq centaur-lsp 'lsp-mode)                   ; Set LSP client: lsp-mode, eglot or nil
