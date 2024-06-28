7bd98eeea220138bacce529f3240676dcdaab1e1
liangl <liispon@gmail.com>
2024-06-28 17:22:51 +0800

    paren

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -82,7 +82,7 @@
                               (when (fboundp 'page-break-lines-mode)
                                 (page-break-lines-mode 1))))
     :init
-    (setq dashboard-banner-logo-title "CENTAUR EMACS - Enjoy Programming & Writing"
+    (setq dashboard-banner-logo-title "EMACS - Enjoy Programming & Writing"
           dashboard-startup-banner (or centaur-logo 'official)
           dashboard-page-separator "\n\f\n"
           dashboard-projects-backend 'project-el
@@ -103,8 +103,9 @@
                                       dashboard-insert-newline
                                       dashboard-insert-init-info
                                       dashboard-insert-items
-                                      dashboard-insert-newline
-                                      dashboard-insert-footer)
+                                      ;; dashboard-insert-newline
+                                      ;; dashboard-insert-footer
+                                      )
 
           dashboard-display-icons-p #'icons-displayable-p
           dashboard-set-file-icons centaur-icon
