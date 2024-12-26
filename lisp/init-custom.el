7f790e349f3ccc45201b44127ff320874a7ff124
l1l <liispon@gmail.com>
2024-12-26 16:28:45 +0800

    package copilot

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -65,7 +65,10 @@
 ;;   "Set HTTP/HTTPS proxy."
 ;;   :group 'centaur
 ;;   :type 'string)
-
+(defcustom centaur-proxy "127.0.0.1:21031"
+  "Set HTTP/HTTPS proxy."
+  :group 'centaur
+  :type 'string)
 ;; (defcustom centaur-socks-proxy "127.0.0.1:1086"
 ;;   "Set SOCKS proxy."
 ;;   :group 'centaur
