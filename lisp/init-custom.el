08c216a9d072dfcbcc28c96b8f006ff789da39f3
liangl <liispon@gmail.com>
2025-09-24 18:08:19 +0800

    l1l

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -60,15 +60,19 @@
   :group 'centaur
   :type 'string)
 
-(defcustom centaur-proxy "127.0.0.1:7897"
+
+;; (defcustom centaur-proxy "127.0.0.1:1087"
+;;   "Set HTTP/HTTPS proxy."
+;;   :group 'centaur
+;;   :type 'string)
+(defcustom centaur-proxy "127.0.0.1:21031"
   "Set HTTP/HTTPS proxy."
   :group 'centaur
   :type 'string)
-
-(defcustom centaur-socks-proxy "127.0.0.1:7897"
-  "Set SOCKS proxy."
-  :group 'centaur
-  :type 'string)
+;; (defcustom centaur-socks-proxy "127.0.0.1:1086"
+;;   "Set SOCKS proxy."
+;;   :group 'centaur
+;;   :type 'string)
 
 (defcustom centaur-server t
   "Enable `server-mode' or not."
